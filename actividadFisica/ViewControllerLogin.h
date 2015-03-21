b8294b8b571dfc7799f10fa7f89c91caf3a497ee
Jorge Perales <jorgeluis070294@hotmail.com>
2015-03-21 16:03:39 -0600

    Pantalla login

---
+++ actividadFisica/ViewControllerLogin.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerLogin.h
+//  actividadFisica
+//
+//  Created by Jorge Luis Perales on 21/03/15.
+//  Copyright (c) 2015 Jorge Luis Perales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerLogin : UIViewController
+
+@end
---
+++ actividadFisica/ViewControllerLogin.m
@@ -0,0 +1,37 @@
+//
+//  ViewControllerLogin.m
+//  actividadFisica
+//
+//  Created by Jorge Luis Perales on 21/03/15.
+//  Copyright (c) 2015 Jorge Luis Perales. All rights reserved.
+//
+
+#import "ViewControllerLogin.h"
+
+@interface ViewControllerLogin ()
+
+@end
+
+@implementation ViewControllerLogin
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
