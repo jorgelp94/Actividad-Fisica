e0f4bf952178b4c8176541c496b4a955af8b6459
Jorge Perales <jorgeluis070294@hotmail.com>
2015-03-21 14:52:16 -0600

    First commit

--- actividadFisica/ViewControllerRegistroCuenta.h
+++ actividadFisica/ViewControllerRegistroCuenta.h
@@ -15,11 +15,17 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imagenPersona;
 @property (weak, nonatomic) IBOutlet UITextField *tfNombre;
 @property (weak, nonatomic) IBOutlet UITextField *tfMatricula;
-@property (weak, nonatomic) IBOutlet UITextField *tfEdad;
 @property (weak, nonatomic) IBOutlet UITextField *tfEstatura;
 @property (weak, nonatomic) IBOutlet UITextField *tfPeso;
+@property (weak, nonatomic) IBOutlet UITextField *tfDia;
+@property (weak, nonatomic) IBOutlet UITextField *tfMes;
+@property (weak, nonatomic) IBOutlet UITextField *tfYear;
+
+
 - (IBAction)preionoCancelar:(UIButton *)sender;
 - (IBAction)presionoCrear:(UIButton *)sender;
 - (IBAction)agregarImagen:(UIButton *)sender;
+
+- (void) quitaTeclado;
 
 @end
--- actividadFisica/ViewControllerRegistroCuenta.m
+++ actividadFisica/ViewControllerRegistroCuenta.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewControllerRegistroCuenta.h"
+#import "Usuario.h"
 
 @interface ViewControllerRegistroCuenta ()
 
@@ -14,11 +15,17 @@
 
 @implementation ViewControllerRegistroCuenta
 
+Usuario *nuevoUsuario;
 
+- (void) quitaTeclado {
+    [self.view endEditing:YES];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(quitaTeclado)];
+    [self.view addGestureRecognizer:tap];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -37,10 +44,23 @@
 */
 
 - (IBAction)preionoCancelar:(UIButton *)sender {
+    
+    // quitar pantalla y mandar a pantalla de login
 }
 
 - (IBAction)presionoCrear:(UIButton *)sender {
+    nuevoUsuario.nombreUsuario = self.tfNombre.text;
+    nuevoUsuario.matriculaUsuario = self.tfMatricula.text;
+    NSString *fecha = [[NSString alloc] initWithFormat:@"%@-%@-%@", self.tfDia.text, self.tfMes.text, self.tfYear.text];
+    //NSLog(@"%@", fecha);
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"dd-mm-yyyy"];
     
+    nuevoUsuario.fechaNacimientoUsuario = [dateFormatter dateFromString:fecha];
+    nuevoUsuario.estaturaUsuario = [self.tfEstatura.text floatValue];
+    nuevoUsuario.pesoUsuario = [self.tfPeso.text floatValue];
+    
+    // quitar pantalla y pasar a la tabbed view
 }
 
 - (IBAction)agregarImagen:(UIButton *)sender {
@@ -53,7 +73,7 @@
 
 - (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     self.imagenPersona.image = [info objectForKey:UIImagePickerControllerOriginalImage];
-    UIImageView *imageView = [[UIImageView alloc] initWithImage:self.imagenPersona.image];
+//    UIImageView *imageView = [[UIImageView alloc] initWithImage:self.imagenPersona.image];
 //    imageView.layer.cornerRadius = self.imagenPersona.image.size.width / 2;
 //    imageView.layer.masksToBounds = YES;
 //    [self.view addSubview:imageView];
