e07d8514ad15daaee8c2de4f80afb6a93e5bdcff
Jorge Perales <jorgeluis070294@hotmail.com>
2015-03-21 12:54:38 -0600

    Initial Commit

---
+++ actividadFisica/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  actividadFisica
+//
+//  Created by Jorge Luis Perales on 21/03/15.
+//  Copyright (c) 2015 Jorge Luis Perales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
